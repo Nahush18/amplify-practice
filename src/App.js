289b181c97f07e4cec48917610773f9b35055481
Nahush Thuse <nahushthuse@gmail.com>
2024-04-25 15:12:18 +0530

    Amplify added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and amplify with me
         </a>
       </header>
     </div>
